c5649a42f3fe4ac29dd04fe7e9eba5f4b54abf23
Andy Johns <ajohns@lexmark.com>
2013-09-26 16:12:38 -0400

    Refactor story class
    The Story class was difficult to test. This initial commit refactors
    the simple components to make them more testable and to reuse code.

--- Story.js
+++ Story.js
@@ -1,19 +1,19 @@
 function Story(data, iterations, releases)
 {
+	this.Release = new SelectBox(data.Release !== null ? data.Release.Name : "", data._ref, releases, 'Release');
+	this.Iteration = new SelectBox(data.Iteration !== null ? data.Iteration.Name : "", data._ref, iterations, 'Iteration');
 	this.model = data;
-	this.iterations = iterations;
-	this.releases = releases;
 	
 	this.display = function () 
 	{        
-		var releaseName = (this.model.Release !== null ? this.model.Release.Name : "");
-		var iterationName = (this.model.Iteration !== null ? this.model.Iteration.Name : "");
-		
 		var tr = document.createElement('tr');
 		tr.className = 'UserStory';
 		tr.id = this.model.ObjectID; //Must give the row an ID to support drag and drop changing the rank
+		tr.appendChild(this.storyField(this.model._ref, '_ref'));
+		tr.appendChild(this.storyField(this.model.Rank, 'Rank'));
+		tr.appendChild(this.storyField(this.model.FormattedID, 'ID'));
 
-		var tdClasses = ['_ref', 'Rank', 'ID', 'Name', 'Points', 'Parent', 'Release', 'Iteration', 'EditLinks'];
+		var tdClasses = ['Name', 'Points', 'Parent', 'Release', 'Iteration', 'EditLinks'];
 		var tds = [];
 
 		//Create td elements and set the class names for each one		
@@ -24,9 +24,6 @@ function Story(data, iterations, releases)
 			tr.appendChild(tds[i]);
 		}
 		
-		tds[0].innerHTML = this.model._ref;
-		tds[1].innerHTML = this.model.Rank;
-		tds[2].innerHTML = this.model.FormattedID;
 		var editLink = new rally.sdk.ui.basic.EditLink
 		(
 			{
@@ -34,78 +31,38 @@ function Story(data, iterations, releases)
 				text: this.model.Name
 			}
 		);
-		editLink.display(tds[3]);
+		editLink.display(tds[0]);
 
-		tds[4].innerHTML = "<input type='text' size='2' name='" + this.model._ref +
+		tds[1].innerHTML = "<input type='text' size='2' name='" + this.model._ref +
 						"' value='" + (this.model.PlanEstimate !== null ? this.model.PlanEstimate : "") + 
 						"' onchange='javascript:updateStory(this.name,\"PlanEstimate\",this.value);'>" + 
 						"</input>";
 
-		tds[5].innerHTML = (this.model.Parent ? this.model.Parent.Name : "");
-		//Make release a select box
-		var releaseHTML = "<select name='" + this.model._ref +
-							"' onchange='javascript:updateStory(this.name," + '"Release"' + ",this.options[this.selectedIndex].value)' " +
-							"onclick='this.focus()'>" +
-							"<option value=''" + (releaseName === "" ? "selected='selected'" : "") + "></option>";
-
-		for(var j=0; j < this.releases.length; ++j)
-		{
-			releaseHTML += "<option value='" + this.releases[j]._ref + "' ";
-			if(releaseName === this.releases[j].Name)
-			{
-				releaseHTML += "selected='selected'";
-			}
-			releaseHTML += ">" + this.releases[j].Name + "</option>";
-        }
-		releaseHTML += "</select>";
-		tds[6].innerHTML = releaseHTML;
-		//Make Iteration a select box
-		var iterationHTML	= "<select name='" + this.model._ref +
-								"' onchange='javascript:updateStory(this.name," + '"Iteration"' + ",this.options[this.selectedIndex].value); " +
-								"calculateVelocity();' onclick='this.focus()'>" +
-								"<option value=''" + (iterationName === "" ? "selected='selected'" : "") + "></option>";
-
-        //If the story's iteration is never found then add it to the selection box (it is in a past iteration)
-        var selected = false;
-		for(j=0; j < this.iterations.length; ++j)
-		{
-			iterationHTML += "<option value='" + this.iterations[j]._ref + "' ";
-            if(iterationName === this.iterations[j].Name)
-            {
-				iterationHTML += "selected='selected'";
-				selected = true;
-            }
-            iterationHTML += ">" + this.iterations[j].Name + "</option>";
-		}
-		if(this.model.Iteration !== null && !selected)
-		{
-			iterationHTML +=	"<option value='" + this.model.Iteration._ref + "' selected='selected'>" +
-								this.model.Iteration.Name + "</option>";
-		}
-		iterationHTML += "</select>";
-		tds[7].innerHTML = iterationHTML;
+		tds[2].innerHTML = (this.model.Parent ? this.model.Parent.Name : "");
+		tds[3].innerHTML = this.Release.display();
+		tds[4].innerHTML = this.Iteration.display();
 
 		//Add an add sibling button if the story is not a defect and has a parent
 		if(this.model._type !== "Defect" && this.model.Parent !== null)
 		{
-			tds[8].innerHTML = "<a title='Create sibling story' style='background:url(https://rally1.rallydev.com/slm/images/sprites.gif) 0 -1139px;'" +
+			tds[5].innerHTML = "<a title='Create sibling story' style='background:url(https://rally1.rallydev.com/slm/images/sprites.gif) 0 -1139px;'" +
 								"href='#' onclick='rally.sdk.util.Navigation.popupCreatePage(\"HierarchicalRequirement\", {parent: " +
 								this.model.Parent.ObjectID + "})'></a>";
 		}
 		//Add an add child button if the story is not a defect
         if(this.model._type !== "Defect")
         {
-			tds[8].innerHTML += "<a title='Create child story' style='background:url(https://rally1.rallydev.com/slm/images/sprites.gif) 0 -1122px;'" +
+			tds[5].innerHTML += "<a title='Create child story' style='background:url(https://rally1.rallydev.com/slm/images/sprites.gif) 0 -1122px;'" +
 								"href='#' onclick='rally.sdk.util.Navigation.popupCreatePage(\"HierarchicalRequirement\", {parent: " +
 								this.model.ObjectID + "})'></a>";
 		}
 		//Add a edit button
-        tds[8].innerHTML += "<a title='Edit story' style='background:url(https://rally1.rallydev.com/slm/images/sprites.gif) 0 -595px;'" +
+        tds[5].innerHTML += "<a title='Edit story' style='background:url(https://rally1.rallydev.com/slm/images/sprites.gif) 0 -595px;'" +
 							"href='#' onclick='rally.sdk.util.Navigation.popupEditPage(\"/" +
 							this.model._type + "/" + this.model.ObjectID + "\")'></a>";
                    
         //Add a delete button
-		tds[8].innerHTML += "<a title='Delete story' style='background:url(https://rally1.rallydev.com/slm/images/sprites.gif) 0 -442px;'" +
+		tds[5].innerHTML += "<a title='Delete story' style='background:url(https://rally1.rallydev.com/slm/images/sprites.gif) 0 -442px;'" +
 							"href='#' onclick='javascript:wsDelete(\"" + this.model.ObjectID + "\", \"" + this.model._type + "\");'></a>";
 
 		return tr;
@@ -117,4 +74,41 @@ function Story(data, iterations, releases)
 		document.getElementById(this.model.ObjectID).childNodes[1].innerHTML = newRank;
 
 	};
+	
+	this.storyField = function (data, className)
+	{
+		var td = document.createElement('td');
+		td.className = className;
+		td.innerHTML = data;
+
+		return td;
+	}
+
+}
+function SelectBox(name, ref, options, key)
+{
+	this._name = name;
+	this._ref = ref;
+	this._options = [{_ref: '', Name: ''}].concat(options);
+	this._key = key;
+	
+	this.display = function()
+	{
+		//Make release a select box
+		var releaseHTML = "<select name='" + this._ref +
+							"' onchange='javascript:updateStory(this.name,\"" + key + "\",this.options[this.selectedIndex].value)' " +
+							"onclick='this.focus()'>";
+							
+		for(var j=0; j < this._options.length; ++j)
+		{
+			releaseHTML += "<option value='" + this._options[j]._ref + "' ";
+			if(this._name === this._options[j].Name)
+			{
+				releaseHTML += "selected='selected'";
+			}
+			releaseHTML += ">" + this._options[j].Name + "</option>";
+        }
+		releaseHTML += "</select>";
+		return releaseHTML;
+	}
 }
--- Tests.js
+++ Tests.js
@@ -1,12 +1,66 @@
+//Story with no parent, release, or iteration
+var storyModel = 
+{
+	_ref: 'https://rally1.rallydev.com/', //URL to a story object
+	ObjectID: '12345', //string
+	Rank: '1', //number
+	FormattedID: 'US12345', //string
+	Name: 'Test Story', //string
+	PlanEstimate: '3', //number
+	Parent: null, //if type is not defect then it can be a story object itself that must have an ObjectID and Name
+	Release: null, //an object that if not null must have a _ref and Name
+	Iteration: null, //an object that if not null must have a _ref and Name
+	_type: 'HierarchicalRequirment'//Defect or Hierarchical requirement
+};
+testStory = new Story(storyModel, [], []);
+
+asyncTest( 'Initialize key test', 1, initializeKeyTest);
+test('Story display test', storyDisplayTest);
+test('Story field display test', storyFieldDisplayTest);
+test('Select box display test', selectBoxDisplayTest);
+
+function storyFieldDisplayTest(assert)
+{
+	var HTML = testStory.storyField('https://rally1.rallydev.com/', '_ref');
+	var expectedHTML = '<td class="_ref">https://rally1.rallydev.com/</td>';
+
+	assert.equal(expectedHTML, HTML.outerHTML, 'Story Field HTML is as expected.');
+}
+
+function selectBoxDisplayTest(assert)
+{
+	var test = new SelectBox('', 'https://rally1.rallydev.com/', [], 'Release');
+	var HTML = test.display();
+	var expectedHTML = "<select name='https://rally1.rallydev.com/' onchange='javascript:updateStory(this.name,\"Release\",this.options[this.selectedIndex].value)' onclick='this.focus()'><option value='' selected='selected'></option></select>";
+	assert.equal(expectedHTML, HTML, 'Story Field HTML is as expected.');
+}
+
+function storyDisplayTest(assert)
+{
+	
+	var tableRow = testStory.display();
+	var expectedHTML = '<td class="_ref">https://rally1.rallydev.com/</td><td class="Rank">1</td><td class="ID">US12345</td><td class="Name"><a href="#">Test Story</a></td><td class="Points"><input size="2" name="https://rally1.rallydev.com/" value="3" onchange="javascript:updateStory(this.name,&quot;PlanEstimate&quot;,this.value);" type="text"></td><td class="Parent"></td><td class="Release"><select name="https://rally1.rallydev.com/" onchange="javascript:updateStory(this.name,&quot;Release&quot;,this.options[this.selectedIndex].value)" onclick="this.focus()"><option value="" selected="selected"></option></select></td><td class="Iteration"><select name="https://rally1.rallydev.com/" onchange="javascript:updateStory(this.name,&quot;Iteration&quot;,this.options[this.selectedIndex].value)" onclick="this.focus()"><option value="" selected="selected"></option></select></td><td class="EditLinks"><a title="Create child story" style="background:url(https://rally1.rallydev.com/slm/images/sprites.gif) 0 -1122px;" href="#" onclick="rally.sdk.util.Navigation.popupCreatePage(&quot;HierarchicalRequirement&quot;, {parent: 12345})"></a><a title="Edit story" style="background:url(https://rally1.rallydev.com/slm/images/sprites.gif) 0 -595px;" href="#" onclick="rally.sdk.util.Navigation.popupEditPage(&quot;/HierarchicalRequirment/12345&quot;)"></a><a title="Delete story" style="background:url(https://rally1.rallydev.com/slm/images/sprites.gif) 0 -442px;" href="#" onclick="javascript:wsDelete(&quot;12345&quot;, &quot;HierarchicalRequirment&quot;);"></a></td>'
+	
+	assert.equal(tableRow.innerHTML, expectedHTML, 'Story innerHTML is as expected.');
+	
+	//Story with a parent
+	//Story without a release
+	//Story with a release
+	//Story without an iteration
+	//Story with an iteration
+	//Story with a list of releases
+	//Story with a list of iterations
+	//Defect
+	//Story that is a parent (I don't show these so not sure how this works)?
+}
+
 function initializeKeyTest(assert)
 {
 	var ws = new RallyWebService(testKey);
 	
 	function testKey(response)
 	{
-		assert.ok(ws.securityKey !== '', "Valid key.");
+		assert.ok(ws.securityKey !== '', 'Valid key.');
 		start();
 	}
 }
-
-asyncTest( 'Initialize key test', 1, initializeKeyTest);
